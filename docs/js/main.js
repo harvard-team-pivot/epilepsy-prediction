2f43772d10c3f9c79f4ea974200068053aead0ba
Shivas Jayaram <shivasj@gmail.com>
2016-12-12 21:09:27 -0500

    Updated Next/Prev links

--- docs/js/main.js
+++ docs/js/main.js
@@ -46,30 +46,30 @@ $(function() {
 
 var current_page_index = 0;
 var sidebar_items = [
-    {"id":"01","page":"components/01-overview.html"}
-    ,{"id":"05","page":"components/05-preface.html"}
-    ,{"id":"10","page":"components/10-data.html"}
-    ,{"id":"20","page":"components/20-explore.html"}
-    ,{"id":"21","page":"ipynb/20_exploratory_data_analysis_01.html"}
-    ,{"id":"22","page":"ipynb/20_exploratory_data_analysis_02.html"}
-    ,{"id":"23","page":"ipynb/20_exploratory_data_analysis_03.html"}
-    ,{"id":"24","page":"ipynb/20_exploratory_data_analysis_04.html"}
-    ,{"id":"30","page":"components/30-data-scrubbing.html"}
-    ,{"id":"31","page":"ipynb/30_data_scrubbing_01.html"}
-    ,{"id":"40","page":"components/40-model.html"}
-    ,{"id":"41","page":"ipynb/40_model_01.html"}
-    ,{"id":"42","page":"ipynb/40_model_02.html"}
-    ,{"id":"43","page":"ipynb/40_model_03.html"}
-    ,{"id":"50","page":"components/50-results.html"}
-    ,{"id":"100","page":"components/100-references.html"}
-    ,{"id":"150","page":"components/150-process.html"}
-    ,{"id":"200","page":"components/200-sitemap.html"}
+    {"id":"01","page":"components/01-overview.html","name":"Overview"}
+    ,{"id":"05","page":"components/05-preface.html","name":"Preface"}
+    ,{"id":"10","page":"components/10-data.html","name":"Data"}
+    ,{"id":"20","page":"components/20-explore.html","name":"Explore"}
+    ,{"id":"21","page":"ipynb/20_exploratory_data_analysis_01.html","name":"Data Source 1"}
+    ,{"id":"22","page":"ipynb/20_exploratory_data_analysis_02.html","name":"Data Source 2"}
+    ,{"id":"23","page":"ipynb/20_exploratory_data_analysis_03.html","name":"Data Source 3"}
+    ,{"id":"24","page":"ipynb/20_exploratory_data_analysis_04.html","name":"Data Source 4"}
+    ,{"id":"30","page":"components/30-data-scrubbing.html","name":"Data Scrubbing"}
+    ,{"id":"31","page":"ipynb/30_data_scrubbing_01.html","name":"Scrubbing Process"}
+    ,{"id":"40","page":"components/40-model.html","name":"Model"}
+    ,{"id":"41","page":"ipynb/40_model_01.html","name":"Base Model"}
+    ,{"id":"42","page":"ipynb/40_model_02.html","name":"Model Performance"}
+    ,{"id":"43","page":"ipynb/40_model_03.html","name":"Final Model"}
+    ,{"id":"50","page":"components/50-results.html","name":"Results"}
+    ,{"id":"100","page":"components/100-references.html","name":"References"}
+    ,{"id":"150","page":"components/150-process.html","name":"Process"}
+    ,{"id":"200","page":"components/200-sitemap.html","name":"Sitemap"}
     ]
 
 function resetpagescroll(){
     $('body').scrollTop(500);
 }
-function openpage(li,id){
+function openpage(li,id,scroll){
     console.log(li)
 
     $('.navbar-nav > li').removeClass("active");
@@ -80,12 +80,18 @@ function openpage(li,id){
     var item = _.find(sidebar_items, function(o) { return o.id == id; });
     if(!item){
         $('#body').load('components/01-overview.html?'+ticks, function() {
-            resetpagescroll();
+            if(scroll){
+                resetpagescroll();
+            }
+
         });
         current_page_index = 0;
     }else{
         $('#body').load(item.page+'?'+ticks, function() {
-            resetpagescroll();
+
+            if(scroll){
+                resetpagescroll();
+            }
 
             // Hide imports section for ipython files
             if(item.page.match(/ipynb*/)){
@@ -107,41 +113,57 @@ function openpage(li,id){
         current_page_index = _.findIndex(sidebar_items, function(o) { return o.id == id; });
     }
     console.log(current_page_index)
-
+    setprevnexttext();
 }
 
 function previouspage(){
     current_page_index--;
     if(current_page_index <= 0){
         current_page_index =0;
-        $('#previous_button').addClass("disabled");
+        $('#previous_button').addClass("hidden");
     }else{
-        $('#previous_button').removeClass("disabled");
+        $('#previous_button').removeClass("hidden");
     }
-    $('#next_button').removeClass("disabled");
+    $('#next_button').removeClass("hidden");
 
     var id = sidebar_items[current_page_index].id;
     var li = $('#li_' + id)[0];
-    openpage(li, id);
+    openpage(li, id,true);
 }
 
 function nextpage(){
     current_page_index++;
     if(current_page_index >= sidebar_items.length-1){
         current_page_index = sidebar_items.length-1;
-        $('#next_button').addClass("disabled");
+        $('#next_button').addClass("hidden");
     }else{
-        $('#next_button').removeClass("disabled");
+        $('#next_button').removeClass("hidden");
     }
-    $('#previous_button').removeClass("disabled");
+    $('#previous_button').removeClass("hidden");
 
     var id = sidebar_items[current_page_index].id;
     var li = $('#li_' + id)[0];
-    openpage(li, id);
+    openpage(li, id,true);
+}
+
+function setprevnexttext(){
+    if(current_page_index == 0){
+        $('#previous_button').addClass("hidden");
+    }else{
+        $('#previous_button a span').text(sidebar_items[current_page_index-1].name);
+    }
+
+    if(current_page_index == sidebar_items.length-1){
+        $('#next_button').addClass("hidden");
+    }else{
+        $('#next_button a span').text(sidebar_items[current_page_index+1].name);
+    }
+
+
 }
 
 $(function() {
-    $('#previous_button').addClass("disabled");
+    //$('#previous_button').addClass("disabled");
 });
 
 
@@ -167,12 +189,14 @@ function loadpagefromurl() {
     // Init with overview page or the page with id
     if (id) {
         var li = $('#li_' + id)[0];
-        openpage(li, id);
+        openpage(li, id,false);
     } else {
         // By default open the overview page
-        $('#body').load('components/01-overview.html?' + ticks, function () {
-            //resetpagescroll();
-        });
+        // $('#body').load('components/01-overview.html?' + ticks, function () {
+        //     //resetpagescroll();
+        // });
+        var li = $('#li_01')[0];
+        openpage(li, id,false);
     }
 }
 
